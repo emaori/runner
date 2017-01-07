b642ece9db0ae1257c7204fba76e886ab6cb0452
emafun <emanuele.origgi@funambol.com>
2017-01-07 12:02:52 +0100

    Initial commit with first implementation
    Please note:
    - This is the very first version. It works but it's very basic :)
    - I haven't tested yet on linux and mac
    - README is not complte: installation section to fill

---
+++ README.md
@@ -0,0 +1,46 @@
+# Runner
+
+`Runner` is an easy peasy python script to run programs according to some conditions.
+The current supported conditions are:
+* day of week
+* time (hh:mm)
+
+Other conditions will be added in the future:
+* platform (mac/linux/windows)
+* enable/disable flag
+
+## Contents
+* [Installation](#installation)
+* [Configuration](#configuration)
+* [Usage](#usage)
+
+## Installation
+
+
+## Configuration
+Edit `runner.json` and add the conditions and the programs to run. For instance, this is the configuration if you want to start notepad every day from 9am to 10pm:
+
+```json
+{
+    "name" : "Name or description of the json file",
+    "schedules" :[
+        {
+            "days" : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            "startTime" : "9.00",
+            "endTime" : "22.00",
+            "tasks" : [
+                "notepad"
+            ]
+        }
+    ]
+}
+```
+
+## Usage
+Execute the python script:
+
+`python runner.py`
+
+By default it takes a file named `runner.json` placed in the same folder of the script. If you want to use a different `.json` file:
+
+`python runner.py alternativeRunner.json`
---
+++ runner.py
@@ -0,0 +1,49 @@
+
+#!/usr/bin/python
+
+import json
+import time
+import calendar
+import subprocess
+import sys
+from datetime import datetime, time
+
+def getTimeTuple(timeStr):
+    return int(timeStr.split('.')[0]), int(timeStr.split('.')[1])
+
+def exeTasks(tasks):
+    for t in tasks:
+        print(">> Going to launch: "+ t)
+        process = subprocess.Popen(t, stdout=subprocess.PIPE)
+
+def exeSchedule(schedule):
+    # Check the day
+    #currentWeekDay = time.strftime("%A")
+    currentWeekDay = calendar.day_name[datetime.today().weekday()]
+    if currentWeekDay in schedule["days"]:
+        # Chck time
+        startTimeTuple = getTimeTuple(schedule["startTime"])
+        endTimeTuple = getTimeTuple(schedule["endTime"])
+        if time(startTimeTuple[0], startTimeTuple[1]) <= datetime.now().time() <= time(endTimeTuple[0], endTimeTuple[1]):
+            # If those match, execute the tasks
+            exeTasks(schedule["tasks"])
+
+# Take the path of the json file from first argument
+# If there is no argument, just use the default (runner.json) file name and
+# the current working directory
+jsonFilePath = "runner.json"
+if len(sys.argv) > 1:
+    jsonFilePath = sys.argv[1]
+
+# Read the json file
+with open(jsonFilePath) as data_file:
+    data = json.load(data_file)
+
+print("Going to execute the json: " + data["name"])
+
+schedules = data["schedules"]
+
+for s in schedules:
+    exeSchedule(s)
+
+print("Runner completed :)")
